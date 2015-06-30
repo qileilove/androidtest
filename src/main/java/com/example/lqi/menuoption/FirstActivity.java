5f14cd4ea2a01e35da839533adf8b10c8f2b1d4d
Lei Qi <lei.qi@rea-group.com>
2015-06-30 11:42:19 +0800

    add the manifest secondactivity intent add android:name="com.example.activitytest.MY_CATEGORY"/> for catgory

--- src/main/java/com/example/lqi/menuoption/FirstActivity.java
+++ src/main/java/com/example/lqi/menuoption/FirstActivity.java
@@ -24,7 +24,8 @@ public class FirstActivity  extends Activity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                intent.addCategory("com.example.activitytest.MY_CATEGORY");
                 startActivity(intent);
             }
         });
